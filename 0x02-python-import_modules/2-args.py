b72ab606bb712d11801146fce61d68e967994f2f
ruizdani301 <ruizdani301@gmail.com>
2021-12-23 15:28:44 -0500

    3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv)
-n -= 1
-if n == 0:
-    print("{} arguments.".format(n))
-else:
-    print("{} arguments:".format(n))
-    for i in range(1, (n+1)):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv)
+    n -= 1
+    if n == 0:
+        print("{} arguments.".format(n))
+    elif n == 1:
+        print("{} argument:".format(n))
+        print("{}: {}".format(n, sys.argv[n]))
+    else:
+        print("{} arguments:".format(n))
+        for i in range(1, (n+1)):
+            print("{}: {}".format(i, sys.argv[i]))
