91c751c73c78fc5fa3e6439a9b2abe34b5daa6d5
ruizdani301 <ruizdani301@gmail.com>
2022-01-04 19:32:39 -0500

    7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    new_tuple = ()
+
+    if len(tuple_a) < 2 or len(tuple_b) < 2:
+        tupla_x = tuple_a + (0, 0)
+        tupla_y = tuple_b + (0, 0)
+        new_tuple = tupla_x[0] + tupla_y[0], tupla_x[1] + tupla_y[1]
+
+    else:
+        new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return new_tuple
