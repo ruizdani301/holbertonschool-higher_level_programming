f90a76870eb6258c6388c55643d10d00d709b554
ruizdani301 <ruizdani301@gmail.com>
2022-01-25 09:17:18 -0500

    all

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-     Area and Perimeter
-     Write a class Rectangle that defines a rectangle
-     by: (based on 1-rectangle.py)
+    Area and Perimeter
+    Write a class Rectangle that defines a rectangle
+    by: (based on 1-rectangle.py)
 """
 
 
 class Rectangle:
     """
-    define Rectangle
-    this module define rectangle
+        define Rectangle
+        this module define rectangle
     """
 
     def __init__(self, width=0, height=0):
@@ -18,53 +18,55 @@ class Rectangle:
 
     @property
     def width(self):
-        """to retrieve width
-           defines a rectangle by: (based on 1-rectangle.py)
+        """
+            to retrieve width
+            defines a rectangle by: (based on 1-rectangle.py)
         """
         return self.__width
 
     @width.setter
     def width(self, value):
         """
-           setter width
-           defines a rectangle by: (based on 1-rectangle.py)
+            setter width
+            defines a rectangle by: (based on 1-rectangle.py)
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("with the message width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """to retrieve height
-           defines a rectangle by: (based on 1-rectangle.py)
+        """
+            to retrieve height
+            defines a rectangle by: (based on 1-rectangle.py)
         """
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-           setter height
-           defines a rectangle by: (based on 1-rectangle.py)
+            setter height
+            defines a rectangle by: (based on 1-rectangle.py)
         """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height the message width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
         """
-           calculate area
-           defines a rectangle by: (based on 1-rectangle.py)
+            calculate area
+            defines a rectangle by: (based on 1-rectangle.py)
         """
         return self.width * self.height
 
     def perimeter(self):
         """
-           calculate perimeter
-           defines a rectangle by: (based on 1-rectangle.py)
+            calculate perimeter
+            defines a rectangle by: (based on 1-rectangle.py)
         """
         if self.width == 0 or self.height == 0:
             return 0
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
         Rectangle.number_of_instances += 1
 
     @property
