0836e20ba66a3d1b819335633f7376832c9c0598
ruizdani301 <ruizdani301@gmail.com>
2022-01-25 17:29:58 -0500

    all

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def magic_string(n)
+    """ print the same """
+    frase = ()
+    for conter in range(0, n)
+        i = 0
+        while i != conter
+            frase.append("{}".format("Bestschool"))
+            i + 1
+        print()
+
+
+if __name__ == __main__
+    magic_string
