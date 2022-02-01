151b1a89b64619cf2be6becc79b0a2569eefd0b3
ruizdani301 <ruizdani301@gmail.com>
2022-02-01 15:58:23 -0500

    from

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""Write a function that
+       open a file
+"""
+
+
 def read_file(filename=""):
+    """Write a function that
+       write a file
+    """
     with open(filename) as f:
         file = f.read()
         print(file)
