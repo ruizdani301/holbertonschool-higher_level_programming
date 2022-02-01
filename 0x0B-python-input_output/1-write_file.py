a57f30543803374e2a5fcea7f605c9df49d0f047
ruizdani301 <ruizdani301@gmail.com>
2022-02-01 15:55:01 -0500

    1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,11 @@
 #!/usrt/bin/python3
+"""Write a function that
+       write a file
+"""
 def write_file(filename="", text=""):
+    """Write a function that
+       write a file
+    """
     with open(filename, 'w') as f:
         w_file = f.write(text)
         f.close
