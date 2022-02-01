75ace08d1c04a79d2965983f5ba7aa5215396659
ruizdani301 <ruizdani301@gmail.com>
2022-01-31 23:41:58 -0500

    1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""
+    this function return
+    True if the object is exactly
+    an instance of the specified class ; otherwise False
+"""
+
 
 class MyList(list):
+    """
+       this function return
+       True if the object is exactly
+       an instance of the specified class ; otherwise False
+    """
 
     def print_sorted(self):
+        """
+           this function return
+        """
         print(sorted(self))
