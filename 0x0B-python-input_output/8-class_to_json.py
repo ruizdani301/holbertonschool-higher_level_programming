0fbdcaa7880e9380c711afabd54d0fce05e56bd8
ruizdani301 <ruizdani301@gmail.com>
2022-02-02 17:53:09 -0500

    8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,11 +2,10 @@
 """Write a function that returns the dictionary description
    with simple data structure
 """
-import json
 
 
 def class_to_json(obj):
     """Write a function that returns the dictionary description
        with simple data structure
     """
-    return json.dumps(obj, default=lambda obj: obj.__dict__)
+    return (obj.__dict__)
