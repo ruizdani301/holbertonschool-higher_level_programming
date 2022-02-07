30e25cc23f2269d331b092effae9f65f3b7b7517
tu nombre <ruizdani301@gmail.com>
2022-02-07 16:40:34 -0500

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,12 +35,10 @@ class Base:
         """writes the JSON string representation
            of list_objs to a file:
         """
+        ls = []
         if list_objs is not None:
-            ls = []
             for i in list_objs:
-               ls.append(cls.to_dictionary(i))
+                ls.append(cls.to_dictionary(i))
             file = cls.__name__ + ".json"
             with open(file, "w+") as f:
                 f.write(cls.to_json_string(ls))
-        else:
-            ls = []
