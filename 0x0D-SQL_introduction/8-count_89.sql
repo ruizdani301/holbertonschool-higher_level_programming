987f4952c81882a4490e613a66f3d1039ac8dc01
tu nombre <ruizdani301@gmail.com>
2022-03-15 09:16:46 -0500

    8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,4 @@
---Write a script that displays the number of records with id = 89 in
---the table first_table
+-- Write a script that displays the number of records with id = 89 in
+-- the table first_table
+
 SELECT COUNT(*) FROM first_table WHERE id=89;
