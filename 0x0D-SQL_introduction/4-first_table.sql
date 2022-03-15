fb5a2f2501addaa1efd27f65de9af7e669d2db5a
tu nombre <ruizdani301@gmail.com>
2022-03-15 09:05:29 -0500

    se cambia comentario

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
--- Write a script that creates a
---table called first_table
+-- create table; shouldn't fail if already exists
+
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
