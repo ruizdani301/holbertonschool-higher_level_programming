da77c82746be7db591c7865f24928bb0a25cdbab
tu nombre <ruizdani301@gmail.com>
2022-03-15 09:19:30 -0500

    correction

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,6 @@
---Write a script that lists the number of records with the
---same score in the table second_table of the database hbtn_0c_0
+-- Write a script that lists the number of records with the
+-- same score in the table second_table of the database hbtn_0c_0
+
 SELECT score, COUNT(score)
 AS number
 FROM second_table
