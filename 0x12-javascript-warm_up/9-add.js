55ce19e400f08ff267e7bfe854d3adf6185feda6
tu nombre <ruizdani301@gmail.com>
2022-04-04 17:25:18 -0500

    all

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -7,8 +7,8 @@ const numx = parseInt(process.argv[2]);
 const numy = parseInt(process.argv[3]);
 
 const pro = (process.argv).lenght;
-if (isNaN(numx) || isNaN(numy) || pro < 3) {
-  console.log('Missing size');
+if (isNaN(numx) || isNaN(numy)) {
+   console.log('Missing size');
 } else {
   console.log(add(numx, numy));
 }
