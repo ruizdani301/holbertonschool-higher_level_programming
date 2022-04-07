9b03641d3dc7d420a64b742c166e59ac551ab88c
tu nombre <ruizdani301@gmail.com>
2022-04-07 10:18:30 -0500

    all

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const list = require("./100-data").list;
+const list = require('./100-data').list;
 
-const new_list = list.map((x, index) => x * index);
+const newList = list.map((x, index) => x * index);
 console.log(list);
-console.log(new_list);
+console.log(newList);
