b1bb54d05aa62e7f54132ced5595c1b51e1aea1a
tu nombre <ruizdani301@gmail.com>
2022-04-12 10:04:04 -0500

    all

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, function (error, results, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    /* const urlDos = []; */
+    const data = (JSON.parse(body).characters);
+
+    for (let i = 0; i < data.length; i++) {
+      let urlDos = ('');
+      urlDos = (data[i]);
+
+      request(urlDos, function (error, results, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          const dataDos = (JSON.parse(body));
+          console.log(dataDos.name);
+        }
+      });
+    }
+  }
+});
