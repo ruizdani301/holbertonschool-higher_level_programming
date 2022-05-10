7880d14c9c354959eeb1acd9101869634de5e85a
tu nombre <ruizdani301@gmail.com>
2022-05-10 18:05:20 -0500

    se modifican condicionales

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,6 +6,7 @@ as an argument and lists all  cities  of that
 state, using the database  hbtn_0e_4_usa
 
 """
+from genericpath import exists
 import MySQLdb
 import sys
 
@@ -14,18 +15,14 @@ if __name__ == "__main__":
     database = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                                passwd=sys.argv[2], db=sys.argv[3])
     cur = database.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities left\
+    cur.execute("SELECT cities.name FROM cities left\
             join states on states.id = cities.state_id\
             WHERE states.name=%s order by states.id ASC;", (sys.argv[4], ))
     filas = cur.fetchall()
     larg = (len(filas))
-    try:
-        if sys.argv[4] in filas[2]:
-            for x in filas:
-                if x[2] == sys.argv[4]:
-                    if larg != 1:
-                        larg = larg - 1
-                        print(x[1], end=", ")
-            print(x[1])
-    except:
-        pass
+    if larg != 0:
+        for x in filas:
+            larg = larg - 1
+            if larg != 0:
+                print(x[0], end=", ")
+        print(x[0])
