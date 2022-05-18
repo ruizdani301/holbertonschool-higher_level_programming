1bb2623cdbcdd6a5ad7972dda90ff14928341761
tu nombre <ruizdani301@gmail.com>
2022-05-18 18:35:54 -0500

    task5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sX POST -H "email:test@gmail.com" "$1"
-curl -sX POST -H "subject:I will always be here for PLD" "$1"
+# sends a POST request to the passed URL, and displays the body of the response
+curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
