6c004d29b89f4a8d53be9e5cc3e4a14899389c7c
tu nombre <ruizdani301@gmail.com>
2022-05-18 15:47:29 -0500

    all

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bash
-# script that takes in a URL, sends a request to that URL
+# script that takes in a URL, sends a request to that URL.
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
