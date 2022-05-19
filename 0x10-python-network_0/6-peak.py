5f00686ac41ce3e788b7ef00d027332d18626756
tu nombre <ruizdani301@gmail.com>
2022-05-18 23:00:11 -0500

    all

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,17 +4,19 @@
 
 def find_peak(list_of_integers):
     """find_peak"""
-    arr = list_of_integers
+    arr = len(list_of_integers)
     if len(list_of_integers) == 0:
         return (None)
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    elif len(list_of_integers) == 2:
+        return max(list_of_integers)
+
+    mitad = int(arr / 2)
+    peak = list_of_integers[mitad]
+    if peak > list_of_integers[mitad - 1] and peak > list_of_integers[mitad + 1]:
+        return peak
+    elif peak < list_of_integers[mitad - 1]:
+        return find_peak(list_of_integers[:mitad])
     else:
-        inicio = 1
-        final = len(list_of_integers) - 1
-        peak = set()
-        for i in range(inicio, final):
-            izq = i - 1
-            der = i + 1
-            if arr[izq] <= arr[i] and arr[der] <= arr[i]:
-                peak.add(list_of_integers[i])
-        peak = list(peak)
-        return(peak)
+        return find_peak(list_of_integers[mitad + 1:])
