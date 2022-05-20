bf88bc32bde50e14709c23cd4c87f426843af891
tu nombre <ruizdani301@gmail.com>
2022-05-19 22:58:04 -0500

    all

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,3 +2,11 @@
 """ Write a Python script that fetches
     https://intranet.hbtn.io/status
 """
+import request
+
+if __name__ == "__main__":
+
+    req = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
