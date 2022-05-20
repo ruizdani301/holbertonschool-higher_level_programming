32d77b753c59e8cc63d91c9b53c948f576af3ea2
tu nombre <ruizdani301@gmail.com>
2022-05-19 23:59:56 -0500

    all

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
+"""script that takes your GitHub credentials
+   (username and password) and uses the GitHub
+   API to display your id
 """
-script that takes your GitHub credentials
-(username and password) and uses the GitHub API
-"""
-from sys import argv
 import requests
-
+from sys import argv
 
 if __name__ == "__main__":
-    url = 'https://swapi.co/api/people'
-    param = {'search': argv[1]}
-    req = requests.get(url, params=param)
-
-    matching_ppl = req.json()
-    print("Number of results: {}".format(matching_ppl.get('count')))
-    for person in matching_ppl.get('results'):
-        print(person.get('name'))
+    url = "https://api.github.com/user"
+    req = requests.Session()
+    req.auth = (argv[1], argv[2])
+    eq = req.get(url)
+    try:
+        print('{}'.format(eq.json()['id']))
+    except:
+        print("None")
